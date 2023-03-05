746c27798524c37cda748c79346102cdf57330c2
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-03-06 01:11:21 +0300

    yeni duzenleme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import axios from 'axios'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Karakter from './components/Karakter'
 
 const App = () => {
@@ -10,21 +11,36 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [people, setPeople] = useState([])
+  const [people, setPeople] = useState([]);
+  const [films, setFilms] = useState([]);
 
   useEffect(() => {
-    axios.get('https://swapi.dev/api/people/').then((res) => {
-      setPeople(res.data)
-      console.log("success!" + res + res.data)
-    })
-  }, [setPeople])
+    axios.get('https://swapi.dev/api/people/')
+      .then((res) => {
+        setPeople(res.data)
+        console.log("success!" + res + res.data)
+      })
+      .catch(err =>
+        console.log(err)
+      )
+    axios.get(`https://swapi.dev/api/films/`)
+      .then((res) => {
+        setFilms(res.data);
+        console.log("success!" + res + res.data)
+      })
+      .catch(err =>
+        console.log(err)
+      )
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
-      <Karakter people={people} />
+      {people && people.map((people, i) => {
+        return (<Karakter key={i} people={people} films={films} />)
+      })}
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,10 +1,17 @@
 // Karakter bileşeniniz buraya gelecek
 import React from 'react'
 
-const Karakter = ({ people }) => {
-  return people.map((e) => {
-    <div>{e.results}</div>
-  })
+const Karakter = ({ people, films, key }) => {
+  return
+  (<div >
+    <p>  name: {people.results.name}</p>
+    <p>   gender: {people.results.gender} </p>
+    <p> height: {people.results.height} </p>
+    <p> mass: {people.results.mass} </p>
+    <p> homeworld: {people.results.homeworld} </p>
+    {people.results}
+  </div>)
+
 }
 
 export default Karakter
